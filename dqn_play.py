05b4c46fea7ae74e7a1bfc3d2ca6d233952c92a3
mmarouen <azzouz.marouen@gmail.com>
2025-04-01 22:35:11 +0200

    fix inference pipeline

--- dqn_play.py
+++ dqn_play.py
@@ -1,75 +1,59 @@
 import gymnasium as gym
 import ale_py
+import yaml
 from gymnasium.wrappers import RecordVideo
 import numpy as np
 import numpy as np
 import typing as tt
 import torch
-import collections
-from lib import wrappers
-from lib import dqn_model
+from lib.commons.wrappers import make_atari_env
+from lib.commons.utils import describe_env
+from lib.qlearn.agents import DQN
 
-games_setup = {
-    "PongNoFrameskip-v4": 
-    {
-        'max_reward': 19.5,
-        'hit_reward': 0,
-        'frame_skip': 4, # frame skipping introduced by the wrappers
-        'running_screen': False # scrolling screen games where the player dies with a zero reward after being hit. scoring is only based on how many target the player hits
-    },
-    "ALE/Riverraid-v5":
-    {
-        'max_reward': 2 * 1e5,
-        'hit_reward': 60,
-        'frame_skip': 4, # frame skipping introduced by the wrappers
-        'running_screen': True # scrolling screen games where the player dies with a zero reward after being hit. scoring is only based on how many target the player hits
-    },
-    "ALE/SpaceInvaders-v5":
-    {
-        'max_reward': 500,
-        'hit_reward': 200, # available rewards are 5, 10, 15, 20, 25, 30, 200 (rare)
-        'frame_skip': 4, # frame skipping introduced by the wrappers
-        'running_screen': True # scrolling screen games where the player dies with a zero reward after being hit. scoring is only based on how many target the player hits
-    },
-    "ALE/BeamRider-v5":
-    {
-        'max_reward': 2000,
-        'hit_reward': 15,
-        'frame_skip': 4, # frame skipping introduced by the wrappers
-        'running_screen': True, # scrolling screen games where the player dies with a zero reward after being hit. scoring is only based on how many target the player hits
-        "terminal_on_life_loss": False,
-        "update_max_hit": True,
-        "total_lives": 3
-    }
-}
 
 if __name__ == "__main__":
-    ENV_NAME = "ALE/BeamRider-v5"
-    map_name = ENV_NAME.replace(r'/', '-')
-    ROOT_FOLDER =  "runs/" + map_name + '-dqn-per_conv_not_solved/'
-    MODEL_FILE = ROOT_FOLDER + map_name + "-best.dat"
-    print(games_setup[ENV_NAME].get('terminal_on_life_loss', True))
-    env = wrappers.make_env(
-        ENV_NAME,
-        render_mode="rgb_array",
-        frame_skip=games_setup[ENV_NAME]['frame_skip'],
-        terminal_on_life_loss=games_setup[ENV_NAME].get('terminal_on_life_loss', True)
-        )
-    env = gym.wrappers.RecordVideo(env, video_folder=ROOT_FOLDER + 'video_play')
-    net = dqn_model.DQN(env.observation_space.shape, env.action_space.n)
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--map-name', type=str, default='Acrobot-v1')
+    parser.add_argument('--folder', type=str, default='DQN-test')
+    args = parser.parse_args()
+
+    with open('config/games.yaml') as f:
+        GAMES = yaml.safe_load(f)
+    is_atari = GAMES[args.map_name].get('atari', False)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    map_name = args.map_name.replace(r'/', '-')
+    ROOT_NAME = f"runs/{map_name}-{args.folder}/"
+    NORM_FILE = ROOT_NAME + map_name + '-normalizers.yaml'
+    MODEL_FILE = ROOT_NAME + map_name + "-best.dat"
+    if not is_atari:
+        eval_env = gym.make(args.map_name, render_mode="rgb_array")
+    else:
+        eval_env = make_atari_env(args.map_name, eval_mode=True, render_mode="rgb_array")
+    eval_env = RecordVideo(eval_env, video_folder=ROOT_NAME + "video_play", name_prefix=args.folder.lower())
+    actions_dim, state_dim, is_discrete = describe_env(eval_env)
+    with open(NORM_FILE) as f:
+        my_dict = yaml.safe_load(f)
+    mean_obs = np.asarray(my_dict['observation_mean'])
+    std_obs = np.asarray(my_dict['observation_std'])
+
     weights = torch.load(MODEL_FILE, map_location=lambda stg, _: stg, weights_only=True)
-    net.load_state_dict(weights)
+    net = DQN(state_dim=state_dim, action_dim=actions_dim, is_atari=is_atari, is_discrete=is_discrete).to(device)
+    net.load_state_dict(torch.load(MODEL_FILE, map_location=device, weights_only=True))
 
-    state, _ = env.reset()
+    state, _ = eval_env.reset()
     total_reward = 0.0
-
+    env_steps = 0
     while True:
-        state_v = torch.tensor(np.expand_dims(state, 0))
-        q_vals = net(state_v).data.numpy()[0]
+        env_steps += 1
+        state = (state - mean_obs)  / std_obs
+        state_v = torch.tensor(np.expand_dims(state, 0), dtype=torch.float32).to(device)
+        q_vals = net.get_value(state_v).detach().cpu().numpy()
         action = int(np.argmax(q_vals))
-        state, reward, is_done, is_trunc, _ = env.step(action)
+        state, reward, is_done, is_trunc, _ = eval_env.step(action)
         total_reward += reward
         if is_done or is_trunc:
             break
-    print("Total reward: %.2f" % total_reward)
-    env.close()
+    print(f"Total reward: {total_reward:.3f}, {env_steps} environment steps")
+    eval_env.close()
